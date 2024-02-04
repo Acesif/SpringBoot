257f48943f8ab67522fce2e4fb9466aeeafed731
Asif Zubayer Palak <36653429+Acesif@users.noreply.github.com>
2024-02-04 18:07:11 +0600

    changes

--- src/main/java/com/mycompany/propertymanagement/controller/PropertyController.java
+++ src/main/java/com/mycompany/propertymanagement/controller/PropertyController.java
@@ -1,14 +1,28 @@
 package com.mycompany.propertymanagement.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mycompany.propertymanagement.dto.PropertyDTO;
+import com.mycompany.propertymanagement.service.PropertyService;
 
 @RestController
 @RequestMapping("/api/v1")
 public class PropertyController {
+
+    @Autowired
+    private PropertyService propertyService;
     @GetMapping("/hola")
     public String sayHello(){
         return "<h1>&</h1>";
+    }
+    @PostMapping("/property")
+    public PropertyDTO saveProperty(@RequestBody PropertyDTO propertyDTO){
+        propertyService.saveProperty(propertyDTO);
+        return propertyDTO;
     }
 }
---
+++ src/main/java/com/mycompany/propertymanagement/service/impl/PropertyServiceImpl.java
@@ -0,0 +1,15 @@
+package com.mycompany.propertymanagement.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.mycompany.propertymanagement.dto.PropertyDTO;
+import com.mycompany.propertymanagement.service.PropertyService;
+
+@Service
+public class PropertyServiceImpl implements PropertyService{
+
+    @Override
+    public PropertyDTO saveProperty(PropertyDTO propertyDTO) {
+        return null;
+    }
+}
